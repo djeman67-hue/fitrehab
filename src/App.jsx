14c3f9461ca595793c5ca44d36cf56c054b9b128
Jérémy Wianni <jeremywianni@MacBook-Air-de-Jeremy.local>
2025-12-09 22:13:10 +0100

    Description claire de ce que tu as fait

--- src/App.jsx
+++ src/App.jsx
@@ -141,9 +141,6 @@ export default function FitRehabApp() {
                 <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-red-600 transition-all group-hover:w-full"></span>
               </button>
             ))}
-            <Button variant="primary" className="ml-4" onClick={() => scrollToSection('suivi')}>
-              Réserver
-            </Button>
           </div>
 
           {/* Bouton Menu Mobile */}
